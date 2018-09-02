4610edfef943bbf7773839e165588d0cda5d73c1
macbookpro <macbookpro@MBP-di-mac.station>
2018-09-02 17:38:13 +0200

    0902

--- dozez/res/js/dozez.js
+++ dozez/res/js/dozez.js
@@ -2,30 +2,31 @@ $(document).ready(function(){
   $("#age").keyup(function(){
     $("#propo").html(
 										 "<small class='asa'>ASA 1/2 o <55y</small><br/>" +
-                     "&#8623; " + (2*$("#age").val()).toFixed() + "-" + (2.5*$("#age").val()).toFixed() + " <small>mg <dfn>[2 - 2.5 mg/kg]</dfn></small>" +
+                     "&#8623; " + (2*$("#age").val()).toFixed() + "-" + (2.5*$("#age").val()).toFixed() + " <small>mg <dfn class='di'>[2 - 2.5 mg/kg]</dfn></small>" +
                      "<br/>" +
-										 "&#8669; " + (0.1*$("#age").val()).toFixed() + "-" + (0.15*$("#age").val()).toFixed() + " <small>mg/min</small> + " + (0.15*$("#age").val()).toFixed() + "-" + (0.45*$("#age").val()).toFixed() + " <small>ml/h <dfn>[0.1-0.15 + 25-75 &gamma;/kg/min]</dfn></small>" +
+										 "&#8669; " + (0.1*$("#age").val()).toFixed() + "-" + (0.15*$("#age").val()).toFixed() + " <small>mg/min</small> + " + (0.15*$("#age").val()).toFixed() + "-" + (0.45*$("#age").val()).toFixed() + " <small>ml/h <dfn class='di'>[0.1-0.15 + 25-75 &gamma;/kg/min]</dfn></small>" +
                      "<br/>" +
 										 "<small class='asa'>ASA 3/4 o >55y</small><br/>" +
-										 "&#8623; " + (1*$("#age").val()).toFixed() + "-" + (1.5*$("#age").val()).toFixed() + " <small>mg  <dfn>[1 - 1.5 mg/kg]</dfn></small>" +
+										 "&#8623; " + (1*$("#age").val()).toFixed() + "-" + (1.5*$("#age").val()).toFixed() + " <small>mg  <dfn class='di'>[1 - 1.5 mg/kg]</dfn></small>" +
                      "<br/>" +
-                     "&#8669; " + (0.08*$("#age").val()).toFixed() + "-" + (0.12*$("#age").val()).toFixed() + " <small>mg/min</small> + " + (0.12*$("#age").val()).toFixed() + "-" + (0.36*$("#age").val()).toFixed() + " <small>ml/h <dfn>[0.08-0.12 + 20-60 &gamma;/kg/min]</dfn></small>" +
+                     "&#8669; " + (0.08*$("#age").val()).toFixed() + "-" + (0.12*$("#age").val()).toFixed() + " <small>mg/min</small> + " + (0.12*$("#age").val()).toFixed() + "-" + (0.36*$("#age").val()).toFixed() + " <small>ml/h <dfn class='di'>[0.08-0.12 + 20-60 &gamma;/kg/min]</dfn></small>" +
 										 "<br/>" +
-										 "<small class='asa'>TIVA</small><small><dfn> Roberts 1-10-8-6 model</dfn></small>" +
+										 "<small class='asa'>TIVA</small><small><dfn class='di'> Roberts 1-10-8-6 model</dfn></small>" +
 										 "<br/>" +
 										 (1*$("#age").val()).toFixed() + "<small>	&#x27AA; </small>" + (10*$("#age").val()).toFixed() + "<small> (0-10&rsquo;) &#x27AB; </small>" + (8*$("#age").val()).toFixed() + "<small> (10-20&rsquo;) &#x27AC; </small>" + (6*$("#age").val()).toFixed() + "<small> (>20&rsquo;) mg/h</small>"
                      );
     $("#nimbex").html(
-                    "&#8623; " + (0.15*$("#age").val()).toFixed() + "-" + (0.2*$("#age").val()).toFixed() + " <small>mg <dfn>[0.15 - 0.2 mg/kg]</dfn></small>" +
+                    "&#8623; " + (0.15*$("#age").val()).toFixed() + "-" + (0.2*$("#age").val()).toFixed() + " <small>mg <dfn class='di'>[0.15 - 0.2 mg/kg]</dfn></small>" +
                     "<br/>" +
-                     "&#8614; " + (0.03*$("#age").val()).toFixed() + " <small>mg q20m <dfn>[0.03 mg/kg]</dfn></small>"
+                     "&#8614; " + (0.03*$("#age").val()).toFixed() + " <small>mg q20m <dfn class='di'>[0.03 mg/kg]</dfn></small>"
                     );
     $("#esmeron").html(
-                    "&#8623; " + (0.45*$("#age").val()).toFixed() + " - " + (0.6*$("#age").val()).toFixed() + " - " + (1.2*$("#age").val()).toFixed() + " <small>mg <dfn>[0.45 - 0.6 - 1.2 (RSI) mg/kg]</dfn></small>" +
+                    "&#8623; " + (0.45*$("#age").val()).toFixed() + " - " + (0.6*$("#age").val()).toFixed() + " - " + (1.2*$("#age").val()).toFixed() + " <small>mg <dfn class='di'>[0.45 - 0.6 - 1.2 (RSI) mg/kg]</dfn></small>" +
                     "<br/>" +
-                     "&#8614; " + (0.1*$("#age").val()).toFixed() + "-" + (0.2*$("#age").val()).toFixed() + " <small>mg <dfn>[0.1 - 0.2 mg/kg]</dfn></small>"
+                     "&#8614; " + (0.1*$("#age").val()).toFixed() + "-" + (0.2*$("#age").val()).toFixed() + " <small>mg <dfn class='di'>[0.1 - 0.2 mg/kg]</dfn></small>"
                     );
     $("#naro").html(
+                    "<dfn class='db'>2mg/kg<br/>Onset: 5 - 15 min; Durata >3 h</dfn>" + 
                     "&bull; 0,2%: " + (1*$("#age").val()).toFixed() + " <small>ml</small>" +
                     "<br/>" +
                      "&bull; 0,375%: " + (0.5333*$("#age").val()).toFixed() + " <small>ml</small>" +
@@ -33,11 +34,15 @@ $(document).ready(function(){
                      "&bull; 0,5%: " + (0.4*$("#age").val()).toFixed() + " <small>ml</small>"
                     );
     $("#carbo").html(
+                    "<dfn class='db'>5mg/kg (7mg/kg + Adrena) - max 400mg<br/>Onset: 3 - 5 min; Durata 45 - 90 min</dfn>" +
                     "&bull; 1%: " + (0.5*$("#age").val()).toFixed() + " <small>ml (max 40ml)</small>" +
                     "<br/>" +
-                     "&bull; 2%: " + (0.25*$("#age").val()).toFixed() + " <small>ml (max 20ml)</small>"
+                     "&bull; 2%: " + (0.25*$("#age").val()).toFixed() + " <small>ml (max 20ml)</small>" +
+                     "<br/>" +
+                     "&bull; 2% + Adrena: " + (0.35*$("#age").val()).toFixed() + " <small>ml (max 20ml)</small>"
                     );
 		$("#chiro").html(
+                    "<dfn class='db'>2mg/kg - max 150mg<br/>Onset: 10 - 15 min; Durata 3 - 10 h</dfn>" + 
                     "&bull; 0,25%: " + (0.8*$("#age").val()).toFixed() + " <small>ml (max 60ml)</small>" +
                     "<br/>" +
                      "&bull; 0,5%: " + (0.4*$("#age").val()).toFixed() + " <small>ml (max 30ml)</small>" +
@@ -74,7 +79,11 @@ $(document).ready(function(){
 	//********END SECTION paracetamolo 
 	
 	// MOSTRA I VALORI DI RIFERIMENTO	AL CLICK DELLO SWITCH
-	$("#checkbox1").click(function() {$("dfn").toggleClass("display-inline");});
+	$("#checkbox1").click(function() {
+    $("dfn.di").toggleClass("display-inline");
+    $("dfn.db").toggleClass("display-block");
+    });
+  
 /*$('#checkbox1').change(function(){
   if($(this).prop("checked")) {
     $("dfn").addClass("display-inline");
